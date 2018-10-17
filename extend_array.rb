f85ae96e870a05d55c267ec17ac9013529a513d0
Mark Conanan <mark.conanan@gmail.com>
2018-10-18 09:40:27 +1000

    sync master

--- extend_array.rb
+++ extend_array.rb
@@ -1,7 +1,25 @@
 class Array
-  def square
-    # create your square method here
+  def initialize(array)
+    @array=array
   end
+  def square
+    @square=[]
+    for number in @array do
+      @square.push(number**2)
+    end
+  end
+  def cube
+    @cube=[]
+    for number in @array do
+      @cube.push(number**3)
+    end
+  end
+  # def average
+  #   @average=(@array.sum)/(@array.length)
+  # end
+  # def sum
+  #   @sum=@array.inject { |sum,n| sum + n }
+  # end
 
   # now fill in the rest
 end
